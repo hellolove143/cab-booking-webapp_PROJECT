65223900e7eecfc378de7864c7ef19a12a53900d
Manas ranjan <108195528+hellolove143@users.noreply.github.com>
2023-03-09 01:12:26 +0530

    Update 2ndversion-of-update.sh

--- 2ndversion-of-update.sh
+++ 2ndversion-of-update.sh
@@ -8,21 +8,21 @@
 #!/bin/bash
 
 # Define the image name
-image="sarojdevops/cab-booking-app::xyz"
+image="hellolove123/cab-booking-app::xyz"
 
 # Remove the tag from the image name
 image=${image%:*}
 
 # Replace the image name in the deployment.yaml file
-sed -i "s|sarojdevops/cab-booking-app.*|$image|g" deployment.yaml
+sed -i "s|hellolove123/cab-booking-app.*|$image|g" deployment.yaml
 
 # Confirm that the change was made
 echo "The tag was removed from the image name in deployment.yaml"
 
 # Get the latest tag of the image
-latest_tag=$(docker images | grep sarojdevops/cab-booking-app | awk '{print $2}' | head -n 1)
+latest_tag=$(docker images | grep hellolove123/cab-booking-app | awk '{print $2}' | head -n 1)
 
 # Update the deployment file with the latest tag
-sed -i "s|sarojdevops/cab-booking-app:.*|sarojdevops/cab-booking-app:$latest_tag|g" deployment.yaml
+sed -i "s|hellolove123/cab-booking-app:.*|hellolove123/cab-booking-app:$latest_tag|g" deployment.yaml
 
 echo "and added new latest tag-versions " : $latest_tag : "in deployment.yaml"
